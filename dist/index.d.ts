935db9d523c540a5e46e8105b8469fe44765676c
Arman2409 <arman.ghazaryan@hylink.am>
2024-03-02 15:21:15 +0400

    added functions built

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,21 +1,18 @@
-type Quarter = 1 | 2 | 3 | 4;
 interface Point {
     x: number;
     y: number;
 }
+type Direction = "left" | "right" | "up" | "down";
 
 declare const distance: (x1: number, y1: number, x2: number, y2: number) => number;
 
 declare const collision: (x1: number, y1: number, x2: number, y2: number, collisionDistance: number, callback?: Function) => boolean;
 
-declare const getMiddlePoint: (x1: number, y1: number, x2: number, y2: number) => {
-    x: number;
-    y: number;
-};
+declare const middle: (x1: number, y1: number, x2: number, y2: number) => Point;
 
 declare const pointWithoutCollision: (minX: number, maxX: number, minY: number, maxY: number, distance: number, others: Point[]) => Point;
 
-declare const collisionInArray: (x: number, y: number, radius: number, arr: Point[], callback?: Function, repeated?: number) => Point | false;
+declare const collisionInArray: (x: number, y: number, radius: number, arr: Point[]) => Point[];
 
 declare const angle: (startX: number, startY: number, endX: number, endY: number) => number;
 
@@ -25,7 +22,7 @@ declare const area: (points: Point[]) => number;
 
 declare const randomPointInDistance: (x: number, y: number, distance: number) => Point;
 
-declare const nearest: (x: number, y: number, points: Point[]) => Point | null;
+declare const nearest: (x: number, y: number, points: Point[]) => Point;
 
 declare const perimeter: (points: Point[]) => number;
 
@@ -41,6 +38,33 @@ declare const farest: (x: number, y: number, points: Point[]) => Point | null;
 
 declare const randomPoint: (xMin?: number, xMax?: number, yMin?: number, yMax?: number) => Point;
 
+declare const move: (x: number, y: number, xStep: number, yStep: number, count: number) => Point[];
+
+declare const scale: (scaleFactorX: number, scaleFactorY: number, points: Point[]) => {
+    x: number;
+    y: number;
+}[];
+
+declare const inLine: (x: number, y: number, point1: Point, point2: Point) => boolean;
+
+declare const cross: (line1Start: Point, line1End: Point, line2Start: Point, line2End: Point) => boolean;
+
+declare const rotate: (centerX: number, centerY: number, points: Point[], angleInRadians: number) => {
+    x: number;
+    y: number;
+}[];
+
+declare const square: (x: number, y: number, size: number, direction?: Direction) => Point[];
+
+declare const rectangle: (x: number, y: number, width: number, height: number, direction?: Direction) => Point[];
+
+declare const triangle: (x: number, y: number, size: number, direction?: Direction) => Point[];
+
+declare const pentagon: (centerX: number, centerY: number, radius: number, angle?: number) => {
+    x: number;
+    y: number;
+}[];
+
 declare const inRange: (number: number, minLimit: number, maxLimit: number) => boolean;
 
 declare const roundToPrecision: (number: number, precision: number) => number;
@@ -48,6 +72,8 @@ declare const roundToPrecision: (number: number, precision: number) => number;
 declare const degreesToRadians: (degrees: number) => number;
 
 declare const radiansToDegrees: (radians: number) => number;
+
+declare const average: (numbers: number[]) => number;
 
 declare const uniqueId: (others?: string[]) => string;
 
@@ -59,36 +85,55 @@ declare const intersection: (arr1: any[], arr2: any[]) => any[];
 
 declare const difference: (arr1: any[], arr2: any[]) => any[];
 
+declare const chunk: (array: any[], perChunk: number) => any[][];
+
 declare const _default: {
-    uniqueId: (others?: string[] | undefined) => string;
     distance: (x1: number, y1: number, x2: number, y2: number) => number;
-    middlePoint: (x1: number, y1: number, x2: number, y2: number) => {
-        x: number;
-        y: number;
-    };
+    middle: (x1: number, y1: number, x2: number, y2: number) => Point;
     collision: (x1: number, y1: number, x2: number, y2: number, collisionDistance: number, callback?: Function | undefined) => boolean;
     pointWithoutCollision: (minX: number, maxX: number, minY: number, maxY: number, distance: number, others: Point[]) => Point;
-    collisionInArray: (x: number, y: number, radius: number, arr: Point[], callback?: Function | undefined, repeated?: number | undefined) => false | Point;
+    collisionInArray: (x: number, y: number, radius: number, arr: Point[]) => Point[];
     angle: (startX: number, startY: number, endX: number, endY: number) => number;
-    inRange: (number: number, minLimit: number, maxLimit: number) => boolean;
-    randomBoolean: () => boolean;
     randomPoint: (xMin?: number | undefined, xMax?: number | undefined, yMin?: number | undefined, yMax?: number | undefined) => Point;
-    randomNumber: (min: number, max: number) => number;
     randomPoints: (xLimitsMin: number, xLimitsMax: number, yLimitsMin: number, yLimitsMax: number, count: number) => Point[];
     area: (points: Point[]) => number;
     circleArea: (radius: number) => number;
     randomPointInDistance: (x: number, y: number, distance: number) => Point;
-    roundToPrecision: (number: number, precision: number) => number;
-    nearest: (x: number, y: number, points: Point[]) => Point | null;
+    nearest: (x: number, y: number, points: Point[]) => Point;
     farest: (x: number, y: number, points: Point[]) => Point | null;
-    degreesToRadians: (degrees: number) => number;
-    radiansToDegrees: (radians: number) => number;
     perimeter: (points: Point[]) => number;
     positionInCircle: (centerX: number, centerY: number, radius: number, angleInRadians: number) => Point;
     possibleConnections: (pointsCount: number) => number;
     center: (points: Point[]) => Point;
+    inLine: (x: number, y: number, point1: Point, point2: Point) => boolean;
+    rotate: (centerX: number, centerY: number, points: Point[], angleInRadians: number) => {
+        x: number;
+        y: number;
+    }[];
+    move: (x: number, y: number, xStep: number, yStep: number, count: number) => Point[];
+    scale: (scaleFactorX: number, scaleFactorY: number, points: Point[]) => {
+        x: number;
+        y: number;
+    }[];
+    cross: (line1Start: Point, line1End: Point, line2Start: Point, line2End: Point) => boolean;
+    square: (x: number, y: number, size: number, direction?: Direction) => Point[];
+    rectangle: (x: number, y: number, width: number, height: number, direction?: Direction) => Point[];
+    triangle: (x: number, y: number, size: number, direction?: Direction) => Point[];
+    pentagon: (centerX: number, centerY: number, radius: number, angle?: number) => {
+        x: number;
+        y: number;
+    }[];
+    uniqueId: (others?: string[] | undefined) => string;
+    randomNumber: (min: number, max: number) => number;
+    randomBoolean: () => boolean;
+    average: (numbers: number[]) => number;
+    inRange: (number: number, minLimit: number, maxLimit: number) => boolean;
+    degreesToRadians: (degrees: number) => number;
+    radiansToDegrees: (radians: number) => number;
+    roundToPrecision: (number: number, precision: number) => number;
     intersection: (arr1: any[], arr2: any[]) => any[];
     difference: (arr1: any[], arr2: any[]) => any[];
+    chunk: (array: any[], perChunk: number) => any[][];
 };
 
-export { type Point, type Quarter, angle, area, center, circleArea, collision, collisionInArray, _default as default, degreesToRadians, difference, distance, farest, inRange, intersection, getMiddlePoint as middlePoint, nearest, perimeter, pointWithoutCollision, positionInCircle, possibleConnections, radiansToDegrees, randomBoolean, randomNumber, randomPoint, randomPointInDistance, randomPoints, roundToPrecision, uniqueId };
+export { type Direction, type Point, angle, area, average, center, chunk, circleArea, collision, collisionInArray, cross, _default as default, degreesToRadians, difference, distance, farest, inLine, inRange, intersection, middle, move, nearest, pentagon, perimeter, pointWithoutCollision, positionInCircle, possibleConnections, radiansToDegrees, randomBoolean, randomNumber, randomPoint, randomPointInDistance, randomPoints, rectangle, rotate, roundToPrecision, scale, square, triangle, uniqueId };
--- dist/index.js
+++ dist/index.js
@@ -7,12 +7,17 @@ var distance = (x1, y1, x2, y2) => {
 var distance_default = distance;
 
 // core/math/inRange.ts
-var inRange = (number, minLimit, maxLimit) => number >= minLimit && number <= maxLimit;
+var inRange = (number, minLimit, maxLimit) => {
+  return number >= minLimit && number <= maxLimit;
+};
 var inRange_default = inRange;
 
 // core/points/collision.ts
 var collision = (x1, y1, x2, y2, collisionDistance, callback) => {
-  if ((inRange_default(x1, x2 - collisionDistance, x2) || inRange_default(x1, x2, x2 + collisionDistance)) && (inRange_default(y1, y2 - collisionDistance, y2) || inRange_default(y1, y2, y2 + collisionDistance))) {
+  if (
+    // Check if the given values are in the range 
+    (inRange_default(x1, x2 - collisionDistance, x2) || inRange_default(x1, x2, x2 + collisionDistance)) && (inRange_default(y1, y2 - collisionDistance, y2) || inRange_default(y1, y2, y2 + collisionDistance))
+  ) {
     callback && callback();
     return true;
   }
@@ -21,8 +26,8 @@ var collision = (x1, y1, x2, y2, collisionDistance, callback) => {
 };
 var collision_default = collision;
 
-// core/points/middlePoint.ts
-var getMiddlePoint = (x1, y1, x2, y2) => {
+// core/points/middle.ts
+var middle = (x1, y1, x2, y2) => {
   const xDistance = Math.abs(x1 - x2);
   const yDistance = Math.abs(y1 - y2);
   const middleX = x2 >= x1 ? xDistance / 2 : -xDistance / 2;
@@ -32,7 +37,7 @@ var getMiddlePoint = (x1, y1, x2, y2) => {
     y: middleY
   };
 };
-var middlePoint_default = getMiddlePoint;
+var middle_default = middle;
 
 // core/points/pointWithoutCollision.ts
 var pointWithoutCollision = (minX, maxX, minY, maxY, distance2, others) => {
@@ -59,70 +64,38 @@ var pointWithoutCollision = (minX, maxX, minY, maxY, distance2, others) => {
 var pointWithoutCollision_default = pointWithoutCollision;
 
 // core/points/collisionInArray.ts
-var collisionInArray = (x, y, radius, arr, callback, repeated) => {
-  if (!repeated)
-    repeated = 0;
-  repeated++;
-  if (repeated >= 100) {
-    console.error("Can't get needed dimesions, returning current dimesions");
-    return {
-      x,
-      y
-    };
-  }
-  let hasCollides = false;
+var collisionInArray = (x, y, radius, arr) => {
   let collisionX = 0;
   let collisionY = 0;
+  const collisionsArr = [];
   arr.forEach(({ x: pointX, y: pointY }) => {
-    if (hasCollides)
-      return;
     collision_default(x, y, pointX, pointY, radius, () => {
-      hasCollides = true;
+      collisionsArr.push({ x: pointX, y: pointY });
       collisionX = pointX;
       collisionY = pointY;
     });
   });
-  if (hasCollides) {
-    const collisionPoint = {
-      x: collisionX,
-      y: collisionY
-    };
-    callback && callback(collisionPoint);
-    return collisionPoint;
-  }
-  return false;
+  return collisionsArr;
 };
 var collisionInArray_default = collisionInArray;
 
 // core/points/angle.ts
 var pi = Math.PI;
 var angle = (startX, startY, endX, endY) => {
-  const xDifference = endX - startX;
-  const yDifference = endY - startY;
-  let toQuarter;
-  if (xDifference >= 0) {
-    if (yDifference >= 0) {
-      toQuarter = 2;
-    } else {
-      toQuarter = 1;
-    }
-  } else {
-    if (yDifference >= 0) {
-      toQuarter = 3;
-    } else {
-      toQuarter = 4;
-    }
+  if (startX === endX && startY === endY) {
+    return 0;
   }
-  const xChange = Math.abs(endX - startX);
-  const yChange = Math.abs(endY - startY);
-  const tan = toQuarter === 1 || toQuarter === 3 ? xChange / yChange : yChange / xChange;
-  const angle_in_radians = (toQuarter - 1) * pi / 2 + Math.atan(tan);
-  return angle_in_radians;
+  const angleInRadians = Math.atan2(endY - startY, endX - startX);
+  const quadrant = Math.ceil((Math.atan2(startY, startX) + 2 * pi) / pi / 2);
+  const adjustedAngle = angleInRadians + (quadrant - 1) * pi / 2;
+  return adjustedAngle;
 };
 var angle_default = angle;
 
 // core/randomization/randomNumber.ts
-var randomNumber = (min, max) => Math.round(Math.random() * (max - min)) + min;
+var randomNumber = (min, max) => {
+  return Math.round(Math.random() * (max - min)) + min;
+};
 var randomNumber_default = randomNumber;
 
 // core/points/randomPoints.ts
@@ -158,14 +131,17 @@ var randomPointInDistance = (x, y, distance2) => {
   const angle2 = Math.random() * 2 * Math.PI;
   const dx = distance2 * Math.cos(angle2);
   const dy = distance2 * Math.sin(angle2);
-  return { x: x + dx, y: y + dy };
+  return {
+    x: x + dx,
+    y: y + dy
+  };
 };
 var randomPointInDistance_default = randomPointInDistance;
 
 // core/points/nearest.ts
 var nearest = (x, y, points) => {
   let minDistance = Infinity;
-  let nearestPoint = null;
+  let nearestPoint = {};
   for (const point of points) {
     const { x: pointX, y: pointY } = point;
     const distanceBetween = distance_default(x, y, pointX, pointY);
@@ -212,11 +188,15 @@ var positionInCircle = (centerX, centerY, radius, angleInRadians) => {
 var positionInCircle_default = positionInCircle;
 
 // core/points/possibleConnections.ts
-var possibleConnections = (pointsCount) => pointsCount * (pointsCount - 1) / 2;
+var possibleConnections = (pointsCount) => {
+  return pointsCount * (pointsCount - 1) / 2;
+};
 var possibleConnections_default = possibleConnections;
 
 // core/points/circleArea.ts
-var circleArea = (radius) => Math.PI * radius ** 2;
+var circleArea = (radius) => {
+  return Math.PI * radius ** 2;
+};
 var circleArea_default = circleArea;
 
 // core/points/center.ts
@@ -252,11 +232,158 @@ var farest = (x, y, points) => {
 var farest_default = farest;
 
 // core/points/randomPoint.ts
-var randomPoint = (xMin, xMax, yMin, yMax) => ({
-  x: xMax ? randomNumber_default(xMin || 0, xMax) : Math.random() * 100,
-  y: yMax ? randomNumber_default(yMin || 0, yMax) : Math.random() * 100
-});
+var randomPoint = (xMin, xMax, yMin, yMax) => {
+  return {
+    x: xMax ? randomNumber_default(xMin || 0, xMax) : Math.random() * 100,
+    y: yMax ? randomNumber_default(yMin || 0, yMax) : Math.random() * 100
+  };
+};
 var randomPoint_default = randomPoint;
+
+// core/points/move.ts
+var move = (x, y, xStep, yStep, count) => {
+  const pointsArr = [];
+  for (let i = 0; i <= count; i++) {
+    const newPoint = {
+      x: x + xStep * i,
+      y: y + yStep * i
+    };
+    pointsArr.push(newPoint);
+  }
+  return pointsArr;
+};
+var move_default = move;
+
+// core/points/scale.ts
+var scale = (scaleFactorX, scaleFactorY, points) => {
+  return points.map(({ x, y }) => ({
+    x: x * scaleFactorX,
+    y: y * scaleFactorY
+  }));
+};
+var scale_default = scale;
+
+// core/points/inLine.ts
+var inLine = (x, y, point1, point2) => {
+  const slope = (point2.y - point1.y) / (point2.x - point1.x);
+  return y - point1.y === slope * (x - point1.x);
+};
+var inLine_default = inLine;
+
+// core/points/cross.ts
+var cross = (line1Start, line1End, line2Start, line2End) => {
+  const line1Dir = {
+    x: line1End.x - line1Start.x,
+    y: line1End.y - line1Start.y
+  };
+  const line2Dir = {
+    x: line2End.x - line2Start.x,
+    y: line2End.y - line2Start.y
+  };
+  const crossProduct = line1Dir.x * line2Dir.y - line1Dir.y * line2Dir.x;
+  if (crossProduct === 0) {
+    return false;
+  }
+  const area1 = (line2End.x - line1Start.x) * (line2Start.y - line1Start.y) - (line2Start.x - line1Start.x) * (line2End.y - line1Start.y);
+  const area2 = (line1End.x - line2Start.x) * (line1Start.y - line2Start.y) - (line1Start.x - line2Start.x) * (line1End.y - line2Start.y);
+  return area1 * area2 < 0;
+};
+var cross_default = cross;
+
+// core/points/rotate.ts
+var rotate = (centerX, centerY, points, angleInRadians) => {
+  return points.map((point) => {
+    const x = centerX + (point.x - centerX) * Math.cos(angleInRadians) - (point.y - centerY) * Math.sin(angleInRadians);
+    const y = centerY + (point.x - centerX) * Math.sin(angleInRadians) + (point.y - centerY) * Math.cos(angleInRadians);
+    return { x, y };
+  });
+};
+var rotate_default = rotate;
+
+// core/points/square.ts
+var sequence = ["right", "down", "left", "up"];
+var steps = {
+  left: [-1, 0],
+  right: [1, 0],
+  up: [0, 1],
+  down: [0, -1]
+};
+var square = (x, y, size, direction = "right") => {
+  const points = [{ x, y }];
+  for (let i = 0; i < 3; i++) {
+    const nextStep = steps[direction];
+    x += nextStep[0] * size;
+    y += nextStep[1] * size;
+    points.push({ x, y });
+    const nextDirectionIndex = sequence.indexOf(direction) + 1 < sequence.length ? sequence.indexOf(direction) + 1 : 0;
+    direction = sequence[nextDirectionIndex];
+  }
+  return points;
+};
+var square_default = square;
+
+// core/points/rectangle.ts
+var sequence2 = ["right", "down", "left", "up"];
+var steps2 = {
+  left: [-1, 0],
+  right: [1, 0],
+  up: [0, 1],
+  down: [0, -1]
+};
+var rectangle = (x, y, width, height, direction = "right") => {
+  console.log({ width, height });
+  const points = [{ x, y }];
+  for (let i = 0; i < 3; i++) {
+    const nextStep = steps2[direction];
+    x += nextStep[0] * width;
+    y += nextStep[1] * height;
+    points.push({ x, y });
+    const nextDirectionIndex = sequence2.indexOf(direction) + 1 < sequence2.length ? sequence2.indexOf(direction) + 1 : 0;
+    direction = sequence2[nextDirectionIndex];
+  }
+  return points;
+};
+var rectangle_default = rectangle;
+
+// core/points/triangle.ts
+var steps3 = {
+  left: [-1, 0],
+  right: [1, 0],
+  up: [0, 1],
+  down: [0, -1]
+};
+var triangle = (x, y, size, direction = "left") => {
+  const height = Math.sqrt(3) / 2 * size;
+  const nextStep = steps3[direction];
+  const points = [{ x, y }];
+  for (let i = 0; i < 2; i++) {
+    if (i === 0) {
+      x += nextStep[0] * size;
+      y += nextStep[1] * size;
+    } else {
+      x += nextStep[0] ? size / 2 : height;
+      y += nextStep[1] ? size / 2 : height;
+    }
+    points.push({ x, y });
+  }
+  return points;
+};
+var triangle_default = triangle;
+
+// core/points/pentagon.ts
+var pentagon = (centerX, centerY, radius, angle2 = 0) => {
+  const points = [];
+  const numberOfSides = 5;
+  angle2 = angle2 % 360;
+  for (let i = 0; i < numberOfSides; i++) {
+    const currentAngle = +(i * (2 * Math.PI)) / numberOfSides;
+    const x = centerX + radius * Math.cos(currentAngle);
+    const y = centerY + radius * Math.sin(currentAngle);
+    points.push({ x, y });
+  }
+  return points;
+};
+var pentagon_default = pentagon;
 
 // core/math/roundToPrecision.ts
 var roundToPrecision = (number, precision) => {
@@ -266,12 +393,22 @@ var roundToPrecision = (number, precision) => {
 var roundToPrecision_default = roundToPrecision;
 
 // core/math/degreesToRadians.ts
-var degreesToRadians = (degrees) => degrees * Math.PI / 180;
+var degreesToRadians = (degrees) => {
+  return degrees * Math.PI / 180;
+};
 var degreesToRadians_default = degreesToRadians;
 
 // core/math/radiansToDegrees.ts
-var radiansToDegrees = (radians) => radians * 180 / Math.PI;
+var radiansToDegrees = (radians) => {
+  return radians * 180 / Math.PI;
+};
 var radiansToDegrees_default = radiansToDegrees;
+
+// core/math/average.ts
+var average = (numbers) => {
+  return numbers.reduce((a, b) => a + b) / numbers.length;
+};
+var average_default = average;
 
 // core/randomization/uniqueId.ts
 var uniqueId = (others) => {
@@ -291,62 +428,101 @@ var uniqueId = (others) => {
 var uniqueId_default = uniqueId;
 
 // core/randomization/randomBoolean.ts
-var randomBoolean = () => Boolean(Math.round(Math.random()));
+var randomBoolean = () => {
+  return Boolean(Math.round(Math.random()));
+};
 var randomBoolean_default = randomBoolean;
 
 // core/arrays/intersection.ts
-var intersection = (arr1, arr2) => arr1.filter((value) => arr2.includes(value));
+var intersection = (arr1, arr2) => {
+  return arr1.filter((value) => arr2.includes(value));
+};
 var intersection_default = intersection;
 
 // core/arrays/difference.ts
-var difference = (arr1, arr2) => arr1.filter((value) => !arr2.includes(value));
+var difference = (arr1, arr2) => {
+  return arr1.filter((value) => !arr2.includes(value));
+};
 var difference_default = difference;
+
+// core/arrays/chunk.ts
+var chunk = (array, perChunk) => {
+  const chunks = [];
+  for (let i = 0; i < array.length / perChunk; i++) {
+    const startIndex = i * perChunk;
+    const newChunk = array.slice(startIndex, startIndex + perChunk);
+    chunks.push(newChunk);
+  }
+  return chunks;
+};
+var chunk_default = chunk;
 
 // index.ts
 var pointscape_default = {
-  uniqueId: uniqueId_default,
+  // points 
   distance: distance_default,
-  middlePoint: middlePoint_default,
+  middle: middle_default,
   collision: collision_default,
   pointWithoutCollision: pointWithoutCollision_default,
   collisionInArray: collisionInArray_default,
   angle: angle_default,
-  inRange: inRange_default,
-  randomBoolean: randomBoolean_default,
   randomPoint: randomPoint_default,
-  randomNumber: randomNumber_default,
   randomPoints: randomPoints_default,
   area: area_default,
   circleArea: circleArea_default,
   randomPointInDistance: randomPointInDistance_default,
-  roundToPrecision: roundToPrecision_default,
   nearest: nearest_default,
   farest: farest_default,
-  degreesToRadians: degreesToRadians_default,
-  radiansToDegrees: radiansToDegrees_default,
   perimeter: perimeter_default,
   positionInCircle: positionInCircle_default,
   possibleConnections: possibleConnections_default,
   center: center_default,
+  inLine: inLine_default,
+  rotate: rotate_default,
+  move: move_default,
+  scale: scale_default,
+  cross: cross_default,
+  square: square_default,
+  rectangle: rectangle_default,
+  triangle: triangle_default,
+  pentagon: pentagon_default,
+  // randomization 
+  uniqueId: uniqueId_default,
+  randomNumber: randomNumber_default,
+  randomBoolean: randomBoolean_default,
+  // math 
+  average: average_default,
+  inRange: inRange_default,
+  degreesToRadians: degreesToRadians_default,
+  radiansToDegrees: radiansToDegrees_default,
+  roundToPrecision: roundToPrecision_default,
+  // arrays 
   intersection: intersection_default,
-  difference: difference_default
+  difference: difference_default,
+  chunk: chunk_default
 };
 export {
   angle_default as angle,
   area_default as area,
+  average_default as average,
   center_default as center,
+  chunk_default as chunk,
   circleArea_default as circleArea,
   collision_default as collision,
   collisionInArray_default as collisionInArray,
+  cross_default as cross,
   pointscape_default as default,
   degreesToRadians_default as degreesToRadians,
   difference_default as difference,
   distance_default as distance,
   farest_default as farest,
+  inLine_default as inLine,
   inRange_default as inRange,
   intersection_default as intersection,
-  middlePoint_default as middlePoint,
+  middle_default as middle,
+  move_default as move,
   nearest_default as nearest,
+  pentagon_default as pentagon,
   perimeter_default as perimeter,
   pointWithoutCollision_default as pointWithoutCollision,
   positionInCircle_default as positionInCircle,
@@ -357,6 +533,11 @@ export {
   randomPoint_default as randomPoint,
   randomPointInDistance_default as randomPointInDistance,
   randomPoints_default as randomPoints,
+  rectangle_default as rectangle,
+  rotate_default as rotate,
   roundToPrecision_default as roundToPrecision,
+  scale_default as scale,
+  square_default as square,
+  triangle_default as triangle,
   uniqueId_default as uniqueId
 };
